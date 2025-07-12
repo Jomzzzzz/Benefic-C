42f5293c8eba06928635f48d103db00a04eaaa21
Jomzzzzz <161435548+Jomzzzzz@users.noreply.github.com>
2025-07-12 18:16:47 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <!-- includes/footer.php -->
-<footer class="bg-white border-t mt-16">
+<footer class="bg-white border-2 border-[#F94144] mt-0">
   <div class="max-w-7xl mx-auto px-4 py-6 text-center">
     <p class="text-sm text-gray-500">
       © <?php echo date("Y"); ?> 
