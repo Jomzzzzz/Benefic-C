474b6a39ba85386cfa91ce33de36cea40c53bc2e
Jomzzzzz <161435548+Jomzzzzz@users.noreply.github.com>
2025-07-12 17:19:07 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -61,7 +61,7 @@
               class="md:hidden text-white bg-[#F94144] hover:bg-red-600 focus:ring-1 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-1 text-center inline-flex items-center"
               type="button">
         Menu
-        <svg class="w-2.5 h-2.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <svg class="w-2 h-2 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
         </svg>
       </button>
